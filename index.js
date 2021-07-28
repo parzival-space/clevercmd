a79c4fdd336c115b6d6904ba458a2ec9a47ed0a1
malte-linke <malte.linke.ml@gmail.com>
2021-07-28 19:45:55 +0200

    added banner and instructions

--- index.js
+++ index.js
@@ -3,18 +3,34 @@
 // import dependencies
 const cleverbot = require('cleverbot-free');
 const readline = require('./readline');
+const { readFileSync } = require('fs');
+const { join } = require('path');
+const { version } = require('./package.json');
 
 
 // save history
 let history = [];
 
 
+// draw banner
+let banner = readFileSync(join(__dirname, 'banner.txt')).toString();
+console.clear();
+console.log(`${banner}\n`);
+console.log(`CleverCMD v${version}`);
+console.log('Type ".quit" to exit\n\nStart a conversation:');
+
+
 // async action loop
 (async () => {
-  while (true) {
+  let hasQuit = false;
+  while (!hasQuit) {
 
     // get user input
     const input = await readline(`> `);
+    if (input === '.quit') {
+      hasQuit = true;
+      continue;
+    }
 
     // send message to cleverbot
     const response = await cleverbot(input, history);
